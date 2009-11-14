4435895c9b6a50523af80a4d587e2043d4f9b92d
mh <mh@immerda.ch>
2009-11-14 15:03:17 +0100

    update to 0.25.x style

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,20 +5,6 @@
 #######################################
 
 
-# modules_dir { "logwatch": }
 class logwatch {
     include logwatch::base
-}
-
-class logwatch::base {
-    package{logwatch:
-        ensure => present,
-    }
-
-    file{"/etc/logwatch/conf/logwatch.conf":
-        source => [ "puppet://$server/modules/logwatch/config/logwatch.conf",
-                    "puppet://$server/files/config/logwatch.conf" ],
-        require => Package[logwatch],
-        mode => 0644, owner => root, group => 0;
-    }
 }
