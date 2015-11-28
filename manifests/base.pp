67d777d2ea917faf966b056790a7fd88854ad983
mh <mh@immerda.ch>
2015-11-28 17:41:34 +0100

    linting for future parser

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,12 +1,15 @@
+# basic logwatch stuff
 class logwatch::base {
-  package{logwatch:
+  package{'logwatch':
     ensure => present,
   }
 
-  file{"/etc/logwatch/conf/logwatch.conf":
-    source => [ "puppet:///modules/site_logwatch/config/logwatch.conf",
-                "puppet:///modules/logwatch/config/logwatch.conf" ],
-    require => Package[logwatch],
-    mode => 0644, owner => root, group => 0;
+  file{'/etc/logwatch/conf/logwatch.conf':
+    source  => [ 'puppet:///modules/site_logwatch/config/logwatch.conf',
+                'puppet:///modules/logwatch/config/logwatch.conf' ],
+    require => Package['logwatch'],
+    owner   => root,
+    group   => 0,
+    mode    => '0644';
   }
 }
