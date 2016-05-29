cb9559874c1353ad560416ad463fbaaf89ccd7b2
mh <mh@immerda.ch>
2016-05-29 14:45:27 +0200

    cleanup for newer puppet support

--- manifests/logserver.pp
+++ manifests/logserver.pp
@@ -1,19 +1,14 @@
 # manifests/logserver.pp
 # used to deploy stuff on the logserver
-
 class logwatch::logserver inherits logwatch::base {
-
-  file{"/opt/bin/logwatch_wrapper.rb":
-    source => "puppet:///modules/logwatch/scripts/logwatch_warpper.rb",
-    require => [ File["/opt/bin"], Package[logwatch] ],
-    mode => 0700, owner => root, group => 0;
+  file{
+    "/opt/bin/logwatch_wrapper.rb":
+      source  => "puppet:///modules/logwatch/scripts/logwatch_warpper.rb",
+      require => [ File["/opt/bin"], Package['logwatch'] ],
+      owner   => root,
+      group   => 0,
+      mode    => '0700';
+    "/etc/cron.daily/logwatch_wrapper.rb":
+      ensure  => "/opt/bin/logwatch_wrapper.rb";
   }
-
-  file{"/etc/cron.daily/logwatch_wrapper.rb":
-    ensure => "/opt/bin/logwatch_wrapper.rb",
-    require => File["/opt/bin/logwatch_wrapper.rb"],
-  }
-
 }
-
-
